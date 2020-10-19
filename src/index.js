84b9625dd30b92cc38ccefdfb5327ec31f66581c
Elbie-em <emoonga1993@gmail.com>
2020-10-19 19:59:47 +0200

    Require logger,middleware and applyMiddler with logger as a param

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
 
-const createStore = redux.createStore
-const combineReducers = redux.combineReducers
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleWare = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
+
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -62,7 +66,7 @@ const rootReducer = combineReducers({
   iceCream : iceCreamReducer
 });
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleWare(logger))
 console.log('Initial state:', store.getState())
 
 const unsubscribe = store.subscribe(() => {
