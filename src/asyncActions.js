dd782e9b7824811c9e13a75bd4ef854beff75da7
Elbie-em <emoonga1993@gmail.com>
2020-10-19 22:07:58 +0200

    Create reducer

--- src/asyncActions.js
+++ src/asyncActions.js
@@ -1,3 +1,5 @@
+
+
 const initialState = {
   loading: false,
   users : [],
@@ -25,5 +27,26 @@ const fetchUsersFailure = (error) => {
   return {
     type: FETCH_USERS_FAILURE,
     payload: error,
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,loading: true
+      }
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: ''
+      }
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users:[],
+        error: action.payload
+      }
   }
 }
