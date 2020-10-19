4e3b4dd935c137d0874e4fe63a7a1d891ef33d77
Elbie-em <emoonga1993@gmail.com>
2020-10-19 22:00:51 +0200

    Add constants for actions, Add action creators

--- src/asyncActions.js
+++ src/asyncActions.js
@@ -4,3 +4,26 @@ const initialState = {
   error: ''
 }
 
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST
+  }
+}
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  }
+}
+
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  }
+}
